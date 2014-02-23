50e1889e93d22881078f3471f81f2a9b9f7e0b59
Juliusz Gonera <jgonera@gmail.com>
2014-02-22 21:23:46 -0800

    Make sure all answers are deleted if question is deleted

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,6 @@ class Question < ActiveRecord::Base
   extend FriendlyId
   friendly_id :text, use: :slugged
 
-  has_many :answers
+  has_many :answers, dependent: :delete_all
   has_one :correct_answer
 end
