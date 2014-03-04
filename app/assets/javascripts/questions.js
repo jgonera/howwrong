ba1e42902399e92ec9522be337ad47ab95e55a64
Moiz Syed <moizsyed@unknown6003088f264a.att.net>
2014-03-04 00:15:14 -0800

    add d3, submit form with radio clicks

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -1,3 +1,6 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+
+$("input[type=radio]").on("change", function() {
+  $(this).closest("form").submit();
+});
+
+$("button").hide();
