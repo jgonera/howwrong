4b7d16509f50210f7e98c3a5392ba08628aff4cb
Moiz Syed <moizsyed@gmail.com>
2014-03-09 23:46:38 -0700

    update css for d3 chart and update d3 chart

--- app/assets/javascripts/results.js
+++ app/assets/javascripts/results.js
@@ -1,7 +1,5 @@
 function drawAnswersGraph() {
   var body = d3.select("body");
-  var div = body.append("div");
-  div.html("Hello chart!");
 
   var count = 0, 
     data = [];
@@ -12,7 +10,7 @@ function drawAnswersGraph() {
   }
   
   var width = 420,
-    barHeight = 20;
+    barHeight = 50;
 
   var x = d3.scale.linear()
       .domain([0, d3.max(data)])
@@ -28,62 +26,14 @@ function drawAnswersGraph() {
       .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
 
   bar.append("rect")
-      .attr("width", x)
-      .attr("height", barHeight - 1);
+      .transition()
+        .attr("width", x)
+        .attr("height", barHeight - 10)
+        .attr("class", "bar");
 
   bar.append("text")
       .attr("x", function(d) { return x(d) - 3; })
-      .attr("y", barHeight / 2)
+      .attr("y", barHeight / 2 - 5)
       .attr("dy", ".35em")
       .text(function(d) { return d; });
 }
-
-
-// <!DOCTYPE html>
-// <meta charset="utf-8">
-// <style>
-
-// .chart rect {
-//   fill: steelblue;
-// }
-
-// .chart text {
-//   fill: white;
-//   font: 10px sans-serif;
-//   text-anchor: end;
-// }
-
-// </style>
-// <svg class="chart"></svg>
-// <script src="http://d3js.org/d3.v3.min.js"></script>
-// <script>
-
-// var data = [4, 8, 15, 16, 23, 42];
-
-// var width = 420,
-//     barHeight = 20;
-
-// var x = d3.scale.linear()
-//     .domain([0, d3.max(data)])
-//     .range([0, width]);
-
-// var chart = d3.select(".chart")
-//     .attr("width", width)
-//     .attr("height", barHeight * data.length);
-
-// var bar = chart.selectAll("g")
-//     .data(data)
-//   .enter().append("g")
-//     .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
-
-// bar.append("rect")
-//     .attr("width", x)
-//     .attr("height", barHeight - 1);
-
-// bar.append("text")
-//     .attr("x", function(d) { return x(d) - 3; })
-//     .attr("y", barHeight / 2)
-//     .attr("dy", ".35em")
-//     .text(function(d) { return d; });
-
-// </script>
