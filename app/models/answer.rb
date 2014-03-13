79928d0747edb0c40f6f59aecedded63906bc9f7
Moiz Syed <moizsyed@gmail.com>
2014-03-12 21:21:37 -0700

    Fix markup for correct answer, add correct scope to answer model

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,6 +2,7 @@ class Answer < ActiveRecord::Base
   belongs_to :question
 
   default_scope { order 'id' }
+  scope :correct, -> { where(is_correct: true).first }
 
   def as_json(options={})
     { id: id, label: label, votes: votes, is_correct: is_correct }
