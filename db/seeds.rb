cfdc36da48882c4c73e2682b278e5ed2631d1c7f
Moiz Syed <moizsyed@gmail.com>
2014-03-12 20:20:30 -0700

    Add votes to default question

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,11 @@ question = Question.create(
   source_url: "https://www.iraqbodycount.org/"
 )
 question.answers.create [
-  { label: "Up to 5,000" },
-  { label: "5,001 — 10,000" },
-  { label: "10,001 — 20,000" },
-  { label: "20,001 — 50,000" },
-  { label: "100,001 — 500,000", is_correct: true },
-  { label: "500,001 — 1,000,000" },
-  { label: "1,000,001+" }
+  { label: "Up to 5,000", votes: 50 },
+  { label: "5,001 — 10,000", votes: 59},
+  { label: "10,001 — 20,000", votes: 21 },
+  { label: "20,001 — 50,000", votes: 10 },
+  { label: "100,001 — 500,000", votes: 45, is_correct: true },
+  { label: "500,001 — 1,000,000", votes: 3 },
+  { label: "1,000,001+", votes: 32 }
 ]
