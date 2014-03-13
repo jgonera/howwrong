c084766a3e8e6addc58360f5c6aa5796e1bd639d
Moiz Syed <moizsyed@gmail.com>
2014-03-12 20:21:19 -0700

    Clean up JavaScript
    Load JS for specific controller actions. Fix turbolinks (use jQuery instead
    of window.onload).

--- app/assets/javascripts/results.js
+++ app/assets/javascripts/results.js
@@ -1,16 +1,9 @@
-function drawAnswersGraph() {
-  var body = d3.select("body");
-
-  var count = 0, 
-    data = [];
-  var numberOfOptions = howwrong.options.answers.length;
-  while (count < numberOfOptions) {
-    data[count] = howwrong.options.answers[count].votes;
-    count++;
-  }
+$(function() {
+  var body = d3.select("body"),
+      data = howwrong.options.answers.map(function(answer) { return answer.votes; });
   
   var width = 420,
-    barHeight = 50;
+      barHeight = 50;
 
   var x = d3.scale.linear()
       .domain([0, d3.max(data)])
@@ -26,14 +19,14 @@ function drawAnswersGraph() {
       .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
 
   bar.append("rect")
-      .transition()
-        .attr("width", x)
-        .attr("height", barHeight - 10)
-        .attr("class", "bar");
+      .attr("width", x)
+      .attr("height", barHeight - 10)
+      .attr("class", "bar");
 
   bar.append("text")
       .attr("x", function(d) { return x(d) - 3; })
       .attr("y", barHeight / 2 - 5)
       .attr("dy", ".35em")
       .text(function(d) { return d; });
-}
+
+});
--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -1,7 +1,7 @@
-function hideButton() {
+$(function() {
   $("input[type=radio]").on("change", function() {
     $(this).closest("form").submit();
   });
 
   $("button").hide();
-}
+});
