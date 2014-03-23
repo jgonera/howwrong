b4907fa3bd54599d812d084bc1a41124fef5ed16
Juliusz Gonera <jgonera@gmail.com>
2014-03-23 00:40:15 -0700

    Fix vote buttons not working after browser back
    Also, make buttons accessible by keyboard by wrapping them in a div
    with role="button".

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -1,5 +1,15 @@
 $(function() {
-  $("input[type=radio]").on("change", function() {
-    $(this).closest("form").submit();
-  });
+  var KEY_ENTER = 13, KEY_SPACE = 32;
+
+  $('[role="button"]')
+    .on("click", function() {
+        $(this).closest("form").submit();
+    })
+    .on("keyup", function(ev) {
+      var $el = $(this);
+      if (ev.which === KEY_ENTER || ev.which === KEY_SPACE) {
+        $el.find("input").prop("checked", true);
+        $el.click();
+      }
+    });
 });
