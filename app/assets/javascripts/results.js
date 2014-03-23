07706cbac24dd36744cc91f7d212cb9cebd50c96
Moiz Syed <moizsyed@gmail.com>
2014-03-22 19:13:53 -0700

    Cleaned up d3 code, added tooltip

--- app/assets/javascripts/results.js
+++ app/assets/javascripts/results.js
@@ -3,13 +3,7 @@
 $(function() {
   var tooltip = d3.select("body")
     .append("div")
-    .append("span")
-    .attr("class", "tooltip")
-    .style("position", "absolute")
-    .style("z-index", "10")
-    .style("visibility", "hidden")
-    .style("background-color", "#000")
-    .text("a simple tooltip");
+    .attr("class", "tooltip");
 
   var body = d3.select("body"),
       data = howwrong.options.answers,
@@ -30,32 +24,32 @@ $(function() {
       .data(data)
     .enter().append("g")
       .attr("transform", function(d, i) {
-        var y = (i * barHeight) + 10;
-       return "translate(0," + (y) +")"; });
+        var y = i * barHeight + 10;
+        return "translate(0," + y +")";
+      })
+      .on("mouseover", function(d, i) {
+        tooltip.style("left", x(d.votes)+700+"px");
+        tooltip.style("top", (i * barHeight) + 10 + 350 +"px");
+        tooltip.classed("visible", true);
+        tooltip.text(d.votes);
+      })
+      .on("mouseout", function() { return tooltip.classed("visible", false); });
 
   bar.append("rect")
-      .attr ("x", 260)
+      .attr("x", 245)
       .attr("width", 0)
       .attr("height", barHeight - 10)
       .classed("bar", true)
       .classed("vote", function(d) { return d.id === vote_answer_id; })
       .classed("correct", function(d) { return d.is_correct; })
-      .on("mouseover", function(d, i) {
-          tooltip.style("left", (d3.mouse(this)[0]+"px"));
-          tooltip.style("top", (d3.mouse(this)[1]+"px"));
-          tooltip.style("visibility", "visible");
-          tooltip.text(d.votes);
-        })
-      .on("mousemove", function(d, i) {
-        tooltip.style("left", ((x(d.votes))+700)+"px");
-        tooltip.style("top", (i * barHeight) + 10 + 350 +"px");
-        console.log(d);
-       })
-      .on("mouseout", function() { return tooltip.style("visibility", "hidden"); })
       .transition()
         .delay(function(d, i) { return i * 50; })
         .duration(1000)
         .attr('width', function(d, i) { return x(d.votes) });
+
+  bar.append("rect")
+      .attr("width", function(d) { return x(d.votes) + 245; })
+      .attr("height", barHeight);
 
   bar.append("text")
       .attr("class", "label")
@@ -65,9 +59,9 @@ $(function() {
       .text(function(d) { return d.label; });
 
   var line = chart.append("line")
-      .attr("x1", 260)
+      .attr("x1", 245)
       .attr("y1", 0)
-      .attr("x2", 260)
+      .attr("x2", 245)
       .attr("y2", 800)
       .attr("style", "stroke:#6b656e; stroke-width:1");
 
