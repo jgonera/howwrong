140ccc6251a10e2d572e959a4e117813b9a61078
Moiz Syed <moizsyed@gmail.com>
2014-04-05 16:01:04 -0700

    Remove some magic numbers, fix tooltip positioning

--- app/assets/javascripts/results.js
+++ app/assets/javascripts/results.js
@@ -1,7 +1,7 @@
 //= require d3.js
 
 $(function() {
-  var tooltip = d3.select("body")
+  var tooltip = d3.select(".chart")
     .append("div")
     .attr("class", "tooltip");
 
@@ -14,28 +14,31 @@ $(function() {
       vote_answer_id = howwrong.options.vote_answer_id,
       votes_count = howwrong.options.votes_count;
 
-  var width = 700,
-      barHeight = 50;
+  var width = 600,
+      barHeight = 40,
+      barGap = 10,
+      labelWidth = 245;
 
   var x = d3.scale.linear()
       .domain([0, d3.max(data, function(answer) { return answer.votes; })])
-      .range([0, width-300]);
+      .range([0, width - labelWidth]);
 
   var chart = d3.select(".chart")
+      .append("svg")
       .attr("width", width)
-      .attr("height", barHeight * data.length + 10);
+      .attr("height", (barHeight + barGap) * data.length + barGap);
 
   var bar = chart.selectAll("g")
       .data(data)
     .enter().append("g")
       .attr("transform", function(d, i) {
-        var y = i * barHeight + 10;
+        var y = i * (barHeight + barGap) + barGap;
         return "translate(0," + y +")";
       })
       .on("mouseover", function(d, i) {
         tooltip
-          .style("left", x(d.votes)+window.innerWidth/2+"px")
-          .style("top", (i * barHeight) + 10 + 525 +"px")
+          .style("left", x(d.votes) + labelWidth + "px")
+          .style("top", i * (barHeight + barGap) + barHeight/2 + "px")
           .classed("visible", true)
           .classed("correct", d.is_correct)
           .classed("wrong", !d.is_correct);
@@ -48,7 +51,7 @@ $(function() {
   bar.append("rect")
       .attr("x", 245)
       .attr("width", 0)
-      .attr("height", barHeight - 10)
+      .attr("height", barHeight)
       .classed("bar", true)
       .classed("vote", function(d) { return d.id === vote_answer_id; })
       .classed("correct", function(d) { return d.is_correct; })
