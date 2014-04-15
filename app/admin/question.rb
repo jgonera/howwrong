710207af2a196d59266280472f85091cb8997833
Moiz Syed <moizsyed@gmail.com>
2014-04-14 19:15:27 -0700

    Questions shit

--- app/admin/question.rb
+++ app/admin/question.rb
@@ -1,9 +1,16 @@
 ActiveAdmin.register Question do
+  permit_params :text, :source, :source_url, :is_featured, :topic
 
   controller do
     def find_resource
       scoped_collection.where(slug: params[:id]).first!
     end
+
+    # def update
+    #   @question = Question.friendly.find(params[:id])
+    #   @question.slug = nil
+    #   @question.update(permitted_params)
+    # end
   end
 
   # See permitted parameters documentation:
