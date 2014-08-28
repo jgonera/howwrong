4ce86720ef7ba0fc01167f85b8061f653447593c
Juliusz Gonera <jgonera@wikimedia.org>
2014-08-27 17:10:17 -0700

    Split feature specs into home page and question page

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 describe "home page" do
   let!(:question) { create :question, text: "Are you wrong?" }
   let!(:wrong_answer) { create :answer, question: question }
-  let!(:another_question) { create :question, text: "Whats your favourite website?" }
   let!(:featured_question) { create :question, text: "Are you featured 1?", is_featured: true }
   let(:featured_answer) { featured_question.answers.first }
 
@@ -15,38 +14,6 @@ describe "home page" do
     expect(page).to have_selector 'h1', text: featured_question.text
     featured_question.answers.each { |answer| expect(page).to have_content answer.label }
     expect(page).to_not have_content wrong_answer.label
-  end
-
-  it "allows voting" do
-    choose featured_answer.label
-    click_button "Submit"
-    featured_answer.reload
-    expect(featured_answer.votes).to eq 1
-  end
-
-  it "allows single click voting if JavaScript enabled", js: true do
-    find('label', text: featured_answer.label.upcase).click
-    featured_answer.reload
-    expect(featured_answer.votes).to eq 1
-  end
-
-  it "marks selected answer in results" do
-    choose featured_answer.label
-    click_button "Submit"
-    answer_row = page.find 'tr', text: featured_answer.label
-    expect(page).to have_selector 'tr.vote', text: featured_answer.label
-  end
-
-  it "gives feedback in results" do
-    choose featured_answer.label
-    click_button "Submit"
-    expect(page).to have_selector 'dd', text: featured_answer.feedback.text
-  end
-
-  it "displays other questions" do
-    expect(page).to have_selector 'li', text: question.text
-    expect(page).to have_selector 'li', text: another_question.text
-    expect(page).to_not have_selector 'li', text: featured_question.text
   end
 
   context "when there are more featured questions" do
---
+++ spec/features/question_page_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+describe "question page" do
+  let!(:question) { create :question, text: "Are you wrong?" }
+  let(:answer) { question.answers.first }
+  let!(:wrong_answer) { create :answer, question: question }
+  let!(:another_question) { create :question, text: "Whats your favourite website?" }
+
+  before :each do
+    visit question_path(question)
+  end
+
+  it "allows voting" do
+    choose answer.label
+    click_button "Submit"
+    answer.reload
+    expect(answer.votes).to eq 1
+  end
+
+  it "allows single click voting if JavaScript enabled", js: true do
+    find('label', text: answer.label.upcase).click
+    answer.reload
+    expect(answer.votes).to eq 1
+  end
+
+  it "marks selected answer in results" do
+    choose answer.label
+    click_button "Submit"
+    expect(page).to have_selector 'tr.vote', text: answer.label
+  end
+
+  it "gives feedback in results" do
+    choose answer.label
+    click_button "Submit"
+    expect(page).to have_selector 'dd', text: answer.feedback.text
+  end
+
+  it "displays other questions" do
+    expect(page).to have_selector 'li', text: another_question.text
+    expect(page).to_not have_selector 'li', text: question.text
+  end
+end
