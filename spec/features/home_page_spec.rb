922a49348fb4d6314b2658f3d18765ae04f11fe2
Juliusz Gonera <juliusz.gonera@airbnb.com>
2014-10-26 20:15:34 -0700

    Reuse question page specs for home page
    It shows a question too.

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,28 +1,40 @@
+require "features/shared_examples/shared_examples_for_question_page"
+
 describe "home page" do
-  let!(:question) { create :question }
-  let!(:featured_question) { create :question, :featured }
+  it_behaves_like "question page" do
+    let!(:question) { create :question, :featured }
 
-  before :each do
-    visit '/'
-  end
-
-  it "displays the latest featured question, its answers and source" do
-    expect(page).to have_selector 'h1', text: featured_question.text
-    featured_question.answers.each { |answer| expect(page).to have_content answer.label }
-  end
-
-  it "doesn't display other question's answers" do
-    expect(page).to_not have_content question.answers.first
-  end
-
-  context "when there are more featured questions" do
-    let!(:another_featured_question) { create :question, :featured }
-
-    it "replaces featured question with a different one after voting" do
-      choose featured_question.answers.first.label
-      click_button "Submit"
+    before :each do
       visit '/'
-      expect(page).to have_selector 'h1', text: another_featured_question.text
+    end
+  end
+
+  context "when featured question present" do
+    let!(:question) { create :question }
+    let!(:featured_question) { create :question, :featured }
+
+    before :each do
+      visit '/'
+    end
+
+    it "displays the latest featured question, its answers and source" do
+      expect(page).to have_selector 'h1', text: featured_question.text
+      featured_question.answers.each { |answer| expect(page).to have_content answer.label }
+    end
+
+    it "doesn't display other question's answers" do
+      expect(page).to_not have_content question.answers.first
+    end
+
+    context "when there are more featured questions" do
+      let!(:another_featured_question) { create :question, :featured }
+
+      it "replaces featured question with a different one after voting" do
+        choose featured_question.answers.first.label
+        click_button "Submit"
+        visit '/'
+        expect(page).to have_selector 'h1', text: another_featured_question.text
+      end
     end
   end
 end
--- spec/features/question_page_spec.rb
+++ spec/features/question_page_spec.rb
@@ -1,40 +1,11 @@
+require "features/shared_examples/shared_examples_for_question_page"
+
 describe "question page" do
-  let!(:question) { create :question, text: "Are you wrong?" }
-  let(:answer) { question.answers.first }
-  let!(:wrong_answer) { create :answer, question: question }
-  let!(:another_question) { create :question, text: "Whats your favourite website?" }
+  it_behaves_like "question page" do
+    let!(:question) { create :question }
 
-  before :each do
-    visit question_path(question)
-  end
-
-  it "allows voting" do
-    choose answer.label
-    click_button "Submit"
-    answer.reload
-    expect(answer.votes).to eq 1
-  end
-
-  it "allows single click voting if JavaScript enabled", js: true do
-    find('label', text: answer.label.upcase).click
-    answer.reload
-    expect(answer.votes).to eq 1
-  end
-
-  it "marks selected answer in results" do
-    choose answer.label
-    click_button "Submit"
-    expect(page).to have_selector 'tr.vote', text: answer.label
-  end
-
-  it "gives feedback in results" do
-    choose answer.label
-    click_button "Submit"
-    expect(page).to have_selector 'dd', text: answer.feedback.text
-  end
-
-  it "displays other questions" do
-    expect(page).to have_selector 'li', text: another_question.text
-    expect(page).to_not have_selector 'li', text: question.text
+    before :each do
+      visit question_path(question)
+    end
   end
 end
