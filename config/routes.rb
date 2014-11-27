b08e9386ce058fb2c83e4eb9a089f6487ecb58c3
Juliusz Gonera <juliusz.gonera@airbnb.com>
2014-11-26 18:27:01 -0800

    Change routes to use singular nouns

--- config/routes.rb
+++ config/routes.rb
@@ -7,15 +7,15 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'questions#index'
 
-  get 'questions/:id', to: 'questions#show', as: :question
-  patch 'questions/:id/vote', to: 'questions#vote', as: :vote_question
-  get 'questions/:id/results', to: 'questions#results', as: :results_questions
+  get 'question/:id', to: 'questions#show', as: :question
+  patch 'question/:id/vote', to: 'questions#vote', as: :vote_question
+  get 'question/:id/results', to: 'questions#results', as: :results_questions
 
-  get 'quizzes/:quiz_id', to: 'quiz_questions#show', as: :quiz
-  get 'quizzes/:quiz_id/:n', to: 'quiz_questions#show', constraints: { n: /\d+/ }, as: :quiz_question
-  patch 'quizzes/:quiz_id/:n/vote', to: 'quiz_questions#vote', constraints: { n: /\d+/ }, as: :vote_quiz_question
-  get 'quizzes/:quiz_id/:n/results', to: 'quiz_questions#results', constraints: { n: /\d+/ }, as: :results_quiz_question
-  get 'quizzes/:quiz_id/results', to: 'quiz_questions#quiz_results', as: :quiz_results
+  get 'quiz/:quiz_id', to: 'quiz_questions#show', as: :quiz
+  get 'quiz/:quiz_id/:n', to: 'quiz_questions#show', constraints: { n: /\d+/ }, as: :quiz_question
+  patch 'quiz/:quiz_id/:n/vote', to: 'quiz_questions#vote', constraints: { n: /\d+/ }, as: :vote_quiz_question
+  get 'quiz/:quiz_id/:n/results', to: 'quiz_questions#results', constraints: { n: /\d+/ }, as: :results_quiz_question
+  get 'quiz/:quiz_id/results', to: 'quiz_questions#quiz_results', as: :quiz_results
 
   get 'about', to: 'pages#about'
   get 'ask', to: 'pages#ask'
