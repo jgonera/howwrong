be787bb1d30224c791b8e4075dbf9a38011e7ce3
Juliusz Gonera <jgonera@gmail.com>
2015-04-14 00:15:22 -0700

    A bit of styling for new question form

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -6,6 +6,22 @@
     d3.select(".vote-form").node().submit();
   }
 
+  function markChecked(el) {
+    d3.select(el.parentNode).selectAll(".checkable").classed("checked", false);
+    d3.select(el).classed("checked", true);
+  }
+
+  d3.selectAll(".checkable")
+    .on("click", function() {
+      markChecked(this);
+    })
+    .on("keyup", function() {
+      var ev = d3.event;
+      if (ev.which === KEY_ENTER || ev.which === KEY_SPACE) {
+        markChecked(this);
+      }
+    });
+
   d3.selectAll('.vote-form [role="button"]')
     .on("click", function() {
       submit(this);
